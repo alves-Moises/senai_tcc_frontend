f8b99807ce11d8b30df020ea396a50c2602099c2
alves-Moises <moisesinho_ra@hotmail.com>
2025-11-27 01:02:11 -0300

    update home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,1 +1,12 @@
- 
+import { Header } from "../../components/Header"
+import { Footer } from "../../components/Footer"
+
+export function Home(){
+    return(
+        <>
+            <Header />
+            <h1>test </h1>
+            <Footer />
+        </>
+    )
+};
