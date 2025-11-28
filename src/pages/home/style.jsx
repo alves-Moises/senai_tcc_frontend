67a2d3d4e50bdc1153c4f460e0218bb6c409b2f5
alves-Moises <moisesinho_ra@hotmail.com>
2025-11-28 02:20:41 -0300

    update style home

---
+++ src/pages/home/style.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: calc(100vh - (74px + 56px)) !important;
+`
