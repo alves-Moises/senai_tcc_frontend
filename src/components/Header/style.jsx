e1cf66375cdea5b27d552ef9d11e9cf3f24f3f43
alves-Moises <moisesinho_ra@hotmail.com>
2025-11-28 02:18:06 -0300

    style header

--- src/components/Header/style.jsx
+++ src/components/Header/style.jsx
@@ -1,5 +1,25 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.header`
+    display: flex;
+    justify-content: space-between;
     background-color: #800066;
+    padding: 20px;
+
+    navbar{
+        display: flex;
+        gap: 20px;
+
+        a{
+            background-color: #345995;
+            color: #dfdfdf;
+            padding: 8px 14px;
+            border-radius: 4px;
+            }
+        a:hover{
+            background-color: #fb4d3d;
+            color: #fff;
+        }
+    }
 `
+
