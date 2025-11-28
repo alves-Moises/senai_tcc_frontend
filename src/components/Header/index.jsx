7b30683cb1b447619914252a6206843a1be95965
alves-Moises <moisesinho_ra@hotmail.com>
2025-11-28 02:17:39 -0300

    update header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,14 @@ import { Container } from "./style"
 export function Header(){
     return(
         <Container>
-            <p>Imagem</p>
+            <a href="/">
+                <img alt="logo" />
+            </a>
+            <navbar>
+                <a href="/">Home</a>
+                <a href="/quizz">Quizz</a>
+                <a href="/records">Records</a>
+            </navbar>
         </Container>
     )
 }
